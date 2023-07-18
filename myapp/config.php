312e13f158f02cb116074e61fab525be2251d8b8
lavanya eswaran <96522510+lavanyaprashola@users.noreply.github.com>
2023-07-18 12:47:03 +0530

    Delete myapp directory

--- myapp/config.php
+++
@@ -1,8 +0,0 @@
-phpCopy code<?php
-
-$config = [
-    'db_host' => '{{ getv "/myapp/database/host" }}',
-    'db_port' => '{{ getv "/myapp/database/port" }}',
-    'db_user' => '{{ getv "/myapp/database/username" }}',
-    'db_pass' => '{{ getv "/myapp/database/password" }}'
-];
